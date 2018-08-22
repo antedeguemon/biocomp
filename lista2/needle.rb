a21db1864eb7f94c5d12841e25853185ea04e31e
antedeguemon <vammerlo@inf.ufrgs.br>
2018-08-21 22:14:25 -0300

    needleing more thigns

--- lista2/needle.rb
+++ lista2/needle.rb
@@ -23,39 +23,39 @@ s_matrix = (0..s1.length).map {(0..s2.length).map { |_| 0 }}
 end
 
 def distance(s, w)
-  align1, align2 = s[0], s[1]
-  score = (0..align1.length-1).map do |i|
-    if align1[i] == align2[i] || (align1[i] != '-' && align2[i] != '-')
-      s(align1[i], align2[i])
+  s1, s2 = s[0], s[1]
+  score = (0..s1.length-1).map do |i|
+    if s1[i] == s2[i] || (s1[i] != '-' && s2[i] != '-')
+      s(s1[i], s2[i])
     else
       w
     end
   end.reduce(0, :+)
-  identity = (0..align1.length-1).select do |i|
-    align1[i] == align2[i]
+  identity = (0..s1.length-1).select do |i|
+    s1[i] == s2[i]
   end.length
 
-  [score, (identity.to_f / align1.length.to_f)]
+  [score, (identity.to_f / s1.length.to_f)]
 end
 
-def traceback(matrix, s1, s2, align1='', align2='', i=s1.length, j=s2.length)
-  return [align1.reverse, align2.reverse] if (i <= 0 && j <= 0)
+def traceback(matrix, s1, s2, a1='', a2='', i=s1.length, j=s2.length)
+  return [a1.reverse, a2.reverse] if (i <= 0 && j <= 0)
   if [i,j].min <= 0
-    (i-1).downto(0).each { |k| align1 += s1[k]; align2 += '-' }
-    (j-1).downto(0).each { |k| align2 += s2[k]; align1 += '-' }
-    return [align1.reverse, align2.reverse]
+    (i-1).downto(0).each { |k| a1 += s1[k]; a2 += '-' }
+    (j-1).downto(0).each { |k| a2 += s2[k]; a1 += '-' }
+    return [a1.reverse, a2.reverse]
   end
 
   cima, esquerda, diagonal = matrix[i][j-1], matrix[i-1][j], matrix[i-1][j-1]
   if matrix[i][j] == diagonal + s(s1[i-1], s2[j-1])
     # diagonal
-    traceback(matrix, s1, s2, align1+s1[i-1], align2+s2[j-1], i-1, j-1)
+    traceback(matrix, s1, s2, a1+s1[i-1], a2+s2[j-1], i-1, j-1)
   elsif matrix[i][j] == esquerda + (-4)
     # esquerda yi alinhado com espaco
-    traceback(matrix, s1, s2, align1+s1[i-1], align2+'-', i-1, j)
+    traceback(matrix, s1, s2, a1+s1[i-1], a2+'-', i-1, j)
   elsif matrix[i][j] == cima + (-4)
     # cima -> xi alinhado com espaco
-    traceback(matrix, s1, s2, align1+'-', align2+s2[j-1], i, j-1)
+    traceback(matrix, s1, s2, a1+'-', a2+s2[j-1], i, j-1)
   end
 end
 
@@ -66,4 +66,5 @@ print "Tabela final de alinhamento:\n"
 }
 alinhadas = traceback(s_matrix, s1, s2)
 print "\nCadeias alinhadas:\n#{alinhadas[0]}\n#{alinhadas[1]}\n"
-print "\nDistancia: #{distance(alinhadas, w)}\n"
+d = distance(alinhadas, w)
+print "\nIdentidade: #{d[0]}\nDistancia: #{d[1]}\n"
